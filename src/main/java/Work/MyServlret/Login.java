d98a8b747e5998c3f9965c426e6a8a3dbf0bc3ae
oleg.shepelia <oleg.shepelia@customertimes.com>
2014-08-14 16:24:16 +0300

    loginForm

--- src/main/java/Work/MyServlret/Login.java
+++ src/main/java/Work/MyServlret/Login.java
@@ -14,6 +14,6 @@ public class Login extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
+        resp.sendRedirect("/LoginForm.jsp");
     }
 }
